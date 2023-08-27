9396810c870850f291e2bad51e2382ee10815248
Eric <18603056530@qq.com>
2023-08-27 13:07:46 +0800

    dev

--- cro-api/src/main/java/com/mk/openai/controller/FileController.java
+++ cro-api/src/main/java/com/mk/openai/controller/FileController.java
@@ -6,14 +6,16 @@ import com.mk.openai.entity.dao.TblFile;
 import com.mk.openai.entity.vo.FileVo;
 import com.mk.openai.exception.ServiceException;
 import com.mk.openai.service.TblFileService;
-import com.mk.openai.service.TblProjectUserService;
 import lombok.AllArgsConstructor;
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.servlet.http.HttpServletResponse;
-import java.io.*;
-import java.net.URLEncoder;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Objects;
 
 @RestController
@@ -31,41 +33,63 @@ public class FileController {
                 loginUserHolder.getCurrentUser().getUserId(), file)).wrapSuccess().build();
     }
 
-    @GetMapping(value = "/download/{fileId}")
-    public void download(@PathVariable Integer fileId, HttpServletResponse response) {
-        try {
-            TblFile fileDao =
-                    tblFileService.getById(fileId);
+    @GetMapping(value = "/show/{fileId}")
+    public ResponseEntity<Resource> show(@PathVariable Integer fileId) throws IOException {
+        TblFile fileDao =
+                tblFileService.getById(fileId);
 
-            if (Objects.isNull(fileDao)) {
-                throw new ServiceException("download file error!");
-            }
-
-            File file = new File(fileDao.getFilePath());
-            String filename = fileDao.getOriginalFilename();
-
-            FileInputStream fileInputStream = new FileInputStream(file);
-            InputStream fis = new BufferedInputStream(fileInputStream);
-            byte[] buffer = new byte[fis.available()];
-            fis.read(buffer);
-            fis.close();
-
-            response.reset();
-            response.setCharacterEncoding("UTF-8");
-            response.addHeader(
-                    "Content-Disposition",
-                    "attachment;filename=" + URLEncoder.encode(filename, "UTF-8"));
-
-            response.addHeader("Content-Length", "" + file.length());
-            response.setHeader("Access-Control-Expose-Headers", "Content-Disposition");
-            OutputStream outputStream = new BufferedOutputStream(response.getOutputStream());
-            response.setContentType("application/octet-stream");
-            outputStream.write(buffer);
-            outputStream.flush();
-        } catch (IOException ex) {
-            ex.printStackTrace();
-            throw new ServiceException("download file error!");
+        if (Objects.isNull(fileDao)) {
+            throw new ServiceException("show file error!");
         }
+
+        Path filePath = Paths.get(fileDao.getFilePath()); // 替换为您的文件路径
+        Resource resource = new org.springframework.core.io.UrlResource(filePath.toUri());
+
+        String filename = fileDao.getOriginalFilename();
+
+        String suffix = filename.substring(filename.lastIndexOf(".")).toLowerCase();
+
+        // 设置响应的Content-Type为文件的MIME类型
+        String contentType = "application/octet-stream";
+
+        switch (suffix) {
+            // 如果是JPEG或JPG图片
+            case ".jpg":
+            case ".jpeg":
+                contentType = "image/jpeg";
+                break;
+            // 如果是PNG图片
+            case ".png":
+                contentType = "image/png";
+                break;
+            // 如果是doc文件
+            case ".doc":
+                contentType = "application/msword";
+                break;
+            // 如果是docx文件
+            case ".docx":
+                contentType = "application/vnd.openxmlformats-officedocument.wordprocessingml.document";
+                break;
+            // 如果是ppt文件
+            case ".ppt":
+                contentType = "application/vnd.ms-powerpoint";
+                break;
+            // 如果是pptx文件
+            case ".pptx":
+                contentType = "application/vnd.openxmlformats-officedocument.presentationml.presentation";
+                break;
+            // 如果是pdf文件
+            case ".pdf":
+                contentType = "application/pdf";
+                break;
+        }
+
+        MediaType mediaType = MediaType.parseMediaType(contentType);
+
+        return ResponseEntity.ok()
+                .contentType(mediaType)
+                .body(resource);
+
     }
 
 
